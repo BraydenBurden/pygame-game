f9728f30c893d9b779bcca191b825114f011729d
Brayden Burden <brayden.burden00@gmail.com>
2022-02-24 11:34:26 -0330

    deleted unused files

--- Fighting-Game/main.py
+++
@@ -1,9 +0,0 @@
-import pygame, sys
-
-GREY = (155, 155, 155)
-WIDTH = 400
-HEIGHT = 300
-
-DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
-DISPLAYSURF.fill(GREY)
-pygame.display.set_caption("Fighting")
